7bcd8f31c4dea5bc03138920e8d43c97fd5d59a3
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-04-27 12:30:05 +0530

    Add files via upload

---
+++ src/components/WeatherDisplay.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WeatherDisplay = ({temperature, conditions}) => {
+  return (
+    <>
+        <p style={temperature < 20 ? {color: 'blue'} : {color: "red"}}>Current Temperature is {temperature} and Condition is {conditions}</p>
+    </>
+  )
+}
+
+export default WeatherDisplay;
